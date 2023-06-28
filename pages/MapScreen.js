f1bc828bb0b963fc3d397e5c196d43a877da71f9
ImHyper01 <57798860+ImHyper01@users.noreply.github.com>
2023-06-28 19:26:52 +0200

    coordinaten goed gezet + andere sneaker stores

--- pages/MapScreen.js
+++ pages/MapScreen.js
@@ -13,64 +13,64 @@ import {
   const Woei = {
     "title": "Woei",
     "description": "Woei is a boutique sneaker store that offers a carefully curated selection of sneakers, clothing, and accessories.",
-    latitude: 51.919151,
-    longitude: 4.479037
+    latitude: 51.92274,
+    longitude: 4.49352
   };
   const Ansh46 = {
     "title": "Ansh46",
       "description": "Ansh46 is a high-end sneaker store featuring a blend of luxury fashion and streetwear brands.",
-      "latitude": 51.920830,
-      "longitude": 4.481102
+      "latitude": 51.91925,
+      "longitude":  4.47421
   };
   const Oqium = {
     "title": "Oqium",
     "description": "Its an sneaker store where u can find special releases and nba merch.",
-    latitude: 51.921884,
-    longitude: 4.475522
+    latitude: 51.92187,
+    longitude: 4.47548
   };
   const Snipes ={
     "title": "Snipes",
     "description": "It's an german base sneaker brand that sells the newest sneakers in the game.",
-    latitude: 51.921113,
-    longitude: 4.476967
+    latitude: 51.92104,
+    longitude: 4.47693
   };
   const FootLocker = {
     "title": "Foot Locker",
     "description": "A well-known sneaker retailer with a branch in Rotterdam, offering a variety of athletic footwear.",
-    latitude: 51.91972,
-    longitude: 4.47778
+    latitude: 51.92097,
+    longitude: 4.47670
   }
   const Pirri = {
     "title": "Pirri",
     "description": "Sells shoes for resell, got the most exclusive sneakers in the game in store.",
-    latitude: 51.91921,
+    latitude: 51.91911,
     longitude: 4.47358
   }
-  const TheAthletesFoot ={
-    "title": "The Athlete's Foot",
-    "description": "The Athlete's Foot is a global retail chain specializing in athletic footwear and sportswear.",
-    "latitude": 51.922983,
-    "longitude": 4.486132
+  const Sport2000 ={
+    "title": "Sport 2000",
+    "description": "The Sport 2000 is a global retail chain specializing in athletic footwear and sportswear.",
+    "latitude": 51.92060,
+    "longitude": 4.48372
   }
   const JdSports = {
     "title": "JD Sports",
     "description": "Its an sport store where u can get most of you sport sneakers to run.",
-    latitude: 51.92132,
-    longitude: 4.47804
+    latitude: 51.91992,
+    longitude: 4.47725
   }
   const SideStep = {
     "title": "Side Step",
     "description": "Discover the latest trends and classic sneaker styles at Side Step in Rotterdam.",
-    latitude: 51.920427,
-    longitude: 4.47734
+    latitude: 51.92026,
+    longitude: 4.47736
   }
-  const Harlem82 = {
-    "title": "Harlem 82",
-    "description": "With a wide range of sneakers and exclusive releases, Sneaker District is a must-visit store in Rotterdam.",
-    latitude: 51.91914,
-    longitude: 4.47361
+  const x21 = {
+    "title": "x21",
+    "description": "With a wide range of sneakers and exclusive cloathing brands, is a must-visit store in Rotterdam.",
+    latitude: 51.92087,
+    longitude: 4.48524
   }
-  
+
    
 const MapScreen = () => {
     return (
@@ -90,10 +90,10 @@ const MapScreen = () => {
       <Marker coordinate={Snipes} ><Image source={require('./Images/snipes.png')} style={{height: 50, width:21.5  }} /></Marker>
       <Marker coordinate={FootLocker} ><Image source={require('./Images/footlockerlogo.png')} style={{height: 30, width: 21.5 }} /></Marker>
       <Marker coordinate={Pirri} ><Image source={require('./Images/pirri.png')} style={{height: 30, width:21.5  }} /></Marker>
-      <Marker coordinate={TheAthletesFoot} ><Image source={require('./Images/athletes.png')} style={{height: 30, width:21.5  }} /></Marker>
+      <Marker coordinate={Sport2000} ><Image source={require('./Images/sport2000.png')} style={{height: 30, width:21.5  }} /></Marker>
       <Marker coordinate={JdSports} ><Image source={require('./Images/jdsport.png')} style={{height: 20, width:21.5  }} /></Marker>
       <Marker coordinate={SideStep} ><Image source={require('./Images/sidesteplogo.png')} style={{height: 50, width:21.5  }} /></Marker>
-      <Marker coordinate={Harlem82} ><Image source={require('./Images/harlem82.png')} style={{height: 20, width:21.5  }} /></Marker>
+      <Marker coordinate={x21} ><Image source={require('./Images/x21.png')} style={{height: 20, width:21.5  }} /></Marker>
      </MapView>
 
      <Text
