172a6438658a375166dcbd9662cb990fd4791521
ImHyper01 <57798860+ImHyper01@users.noreply.github.com>
2023-06-28 18:50:02 +0200

    map functie toegevoegd en markers met fotos gemaakt

--- pages/MapScreen.js
+++ pages/MapScreen.js
@@ -10,124 +10,90 @@ import {
   } from 'react-native';
 
   
-  const BeachClub = {
-    "title": "Sneaker World",
-    "description": "A leading sneaker store in Rotterdam offering a wide range of popular brands.",
-    "latitude": 51.9225,
-    "longitude": 4.47917
+  const Woei = {
+    "title": "Woei",
+    "description": "Woei is a boutique sneaker store that offers a carefully curated selection of sneakers, clothing, and accessories.",
+    latitude: 51.919151,
+    longitude: 4.479037
   };
-  const TwoSeaons = {
-    "title": "Kicks Avenue",
-    "description": "Discover the latest sneaker releases and limited editions at Kicks Avenue in Rotterdam.",
-    "latitude": 51.91833,
-    "longitude": 4.47667
+  const Ansh46 = {
+    "title": "Ansh46",
+      "description": "Ansh46 is a high-end sneaker store featuring a blend of luxury fashion and streetwear brands.",
+      "latitude": 51.920830,
+      "longitude": 4.481102
   };
-  const Villa = {
-    "title": "SoleJunction",
-    "description": "Find your perfect pair of sneakers at Sole Junction, a trendy store located in the heart of Rotterdam.",
-    "latitude": 51.92222,
-    "longitude": 4.48028
+  const Oqium = {
+    "title": "Oqium",
+    "description": "Its an sneaker store where u can find special releases and nba merch.",
+    latitude: 51.921884,
+    longitude: 4.475522
   };
-  const Bing = {
-    latitude:  51.98733719450499,
-    longitude: 4.103833120679355,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
+  const Snipes ={
+    "title": "Snipes",
+    "description": "It's an german base sneaker brand that sells the newest sneakers in the game.",
+    latitude: 51.921113,
+    longitude: 4.476967
   };
-  const FF = {
-    latitude: 51.98703266371214,
-    longitude: 4.103351195779682,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01,
-  };
-
-
-
-  // {
-  //   "stores": [
-  //     {
-  //       "title": "Sneaker World",
-  //       "description": "A leading sneaker store in Rotterdam offering a wide range of popular brands.",
-  //       "latitude": 51.9225,
-  //       "longitude": 4.47917
-  //     },
-  //     {
-  //       "title": "Kicks Avenue",
-  //       "description": "Discover the latest sneaker releases and limited editions at Kicks Avenue in Rotterdam.",
-  //       "latitude": 51.91833,
-  //       "longitude": 4.47667
-  //     },
-  //     {
-  //       "title": "Sole Junction",
-  //       "description": "Find your perfect pair of sneakers at Sole Junction, a trendy store located in the heart of Rotterdam.",
-  //       "latitude": 51.92222,
-  //       "longitude": 4.48028
-  //     },
-  //     {
-  //       "title": "Sneaker Central",
-  //       "description": "Visit Sneaker Central for a wide selection of sneakers and streetwear in Rotterdam.",
-  //       "latitude": 51.92056,
-  //       "longitude": 4.4775
-  //     },
-  //     {
-  //       "title": "Foot Locker",
-  //       "description": "A well-known sneaker retailer with a branch in Rotterdam, offering a variety of athletic footwear.",
-  //       "latitude": 51.91972,
-  //       "longitude": 4.47778
-  //     },
-  //     {
-  //       "title": "Urban Kicks",
-  //       "description": "Experience the urban sneaker culture at Urban Kicks, located in a vibrant neighborhood of Rotterdam.",
-  //       "latitude": 51.92444,
-  //       "longitude": 4.4825
-  //     },
-  //     {
-  //       "title": "Sneaker Spot",
-  //       "description": "Sneaker Spot offers a curated selection of sneakers and accessories in Rotterdam.",
-  //       "latitude": 51.9175,
-  //       "longitude": 4.47444
-  //     },
-  //     {
-  //       "title": "Fresh Soles",
-  //       "description": "Fresh Soles is the go-to destination for sneaker enthusiasts in Rotterdam.",
-  //       "latitude": 51.91667,
-  //       "longitude": 4.47139
-  //     },
-  //     {
-  //       "title": "Sneak Avenue",
-  //       "description": "Discover the latest trends and classic sneaker styles at Sneak Avenue in Rotterdam.",
-  //       "latitude": 51.92139,
-  //       "longitude": 4.47639
-  //     },
-  //     {
-  //       "title": "Sneaker District",
-  //       "description": "With a wide range of sneakers and exclusive releases, Sneaker District is a must-visit store in Rotterdam.",
-  //       "latitude": 51.92194,
-  //       "longitude": 4.47472
-  //     }
-  //   ]
-  // }
+  const FootLocker = {
+    "title": "Foot Locker",
+    "description": "A well-known sneaker retailer with a branch in Rotterdam, offering a variety of athletic footwear.",
+    latitude: 51.91972,
+    longitude: 4.47778
+  }
+  const Pirri = {
+    "title": "Pirri",
+    "description": "Sells shoes for resell, got the most exclusive sneakers in the game in store.",
+    latitude: 51.91921,
+    longitude: 4.47358
+  }
+  const TheAthletesFoot ={
+    "title": "The Athlete's Foot",
+    "description": "The Athlete's Foot is a global retail chain specializing in athletic footwear and sportswear.",
+    "latitude": 51.922983,
+    "longitude": 4.486132
+  }
+  const JdSports = {
+    "title": "JD Sports",
+    "description": "Its an sport store where u can get most of you sport sneakers to run.",
+    latitude: 51.92132,
+    longitude: 4.47804
+  }
+  const SideStep = {
+    "title": "Side Step",
+    "description": "Discover the latest trends and classic sneaker styles at Side Step in Rotterdam.",
+    latitude: 51.920427,
+    longitude: 4.47734
+  }
+  const Harlem82 = {
+    "title": "Harlem 82",
+    "description": "With a wide range of sneakers and exclusive releases, Sneaker District is a must-visit store in Rotterdam.",
+    latitude: 51.91914,
+    longitude: 4.47361
+  }
   
-
-
-
+   
 const MapScreen = () => {
     return (
    <View style={styles.container}>
      <MapView
        style={styles.map}
        region={{
-         latitude: 51.99098,
-         longitude: 4.10808,
+         latitude: 51.91972,
+         longitude: 4.47778,
          latitudeDelta: 0.015,
          longitudeDelta: 0.0121,
        }}
      >
-      <Marker coordinate={BeachClub}><Image source={require('./Images/pngwing.png')} style={{height: 50, width:21.5  }} /></Marker>
-      <Marker coordinate={Bing} />
-      <Marker coordinate={FF} />
-      <Marker coordinate={Villa} />
-      <Marker coordinate={TwoSeaons} />
+      <Marker coordinate={Woei}><Image source={require('./Images/Woei.png')} style={{height: 20, width:21.5  }} /></Marker>
+      <Marker coordinate={Ansh46}><Image source={require('./Images/anhs46.jpg')} style={{height: 20, width:21.5  }} /></Marker> 
+      <Marker coordinate={Oqium} ><Image source={require('./Images/oqium.png')} style={{height: 20, width:21.5  }} /></Marker>
+      <Marker coordinate={Snipes} ><Image source={require('./Images/snipes.png')} style={{height: 50, width:21.5  }} /></Marker>
+      <Marker coordinate={FootLocker} ><Image source={require('./Images/footlockerlogo.png')} style={{height: 30, width: 21.5 }} /></Marker>
+      <Marker coordinate={Pirri} ><Image source={require('./Images/pirri.png')} style={{height: 30, width:21.5  }} /></Marker>
+      <Marker coordinate={TheAthletesFoot} ><Image source={require('./Images/athletes.png')} style={{height: 30, width:21.5  }} /></Marker>
+      <Marker coordinate={JdSports} ><Image source={require('./Images/jdsport.png')} style={{height: 20, width:21.5  }} /></Marker>
+      <Marker coordinate={SideStep} ><Image source={require('./Images/sidesteplogo.png')} style={{height: 50, width:21.5  }} /></Marker>
+      <Marker coordinate={Harlem82} ><Image source={require('./Images/harlem82.png')} style={{height: 20, width:21.5  }} /></Marker>
      </MapView>
 
      <Text
@@ -136,7 +102,7 @@ const MapScreen = () => {
               textAlign: 'center',
               marginBottom: -125
             }}>
-            You are on Map Screen
+            Welkom op de map, zie hier boven alle sneakers stores
           </Text>
    </View>
     );
